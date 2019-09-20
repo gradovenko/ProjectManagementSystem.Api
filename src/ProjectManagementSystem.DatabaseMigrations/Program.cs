9d65aad10d17a341b4db0a513278f108e6dd1b5a
artem.gradovenko <artem.gradovenko@avalab.io>
2019-09-20 15:56:41 +0500

    Add background service

--- src/ProjectManagementSystem.DatabaseMigrations/Program.cs
+++ src/ProjectManagementSystem.DatabaseMigrations/Program.cs
@@ -1,93 +1,47 @@
-﻿using System;
-using System.IO;
-using System.Threading;
+﻿using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Hosting;
 using Serilog;
 
 namespace ProjectManagementSystem.DatabaseMigrations
 {
     internal sealed class Program
     {
-        private static readonly ManualResetEventSlim _manualResetEventSlim = new ManualResetEventSlim(false);
-        
-        private static void Main(string[] args)
+        private static async Task Main(string[] args)
         {
-            var configuration = CreateConfiguration();
-            var serviceProvider = ConfigureServices(configuration);
-            var sp = serviceProvider.BuildServiceProvider();
-            var loggerFactory = sp.GetService<ILoggerFactory>();
-            var logger = loggerFactory.CreateLogger<ProjectManagementSystemDbContext>();
-
-            using (var cancellationToken = new CancellationTokenSource())
-            {
-                void Shutdown()
+            var host = new HostBuilder()
+                .ConfigureAppConfiguration(builder =>
                 {
-                    if (!cancellationToken.IsCancellationRequested)
-                    {
-                        logger.LogInformation("Application is shutting down");
-                        cancellationToken.Cancel();
-                    }
-
-                    _manualResetEventSlim.Wait(cancellationToken.Token);
-                }
-
-                Console.CancelKeyPress += (sender, eventArgs) =>
-                {
-                    Shutdown();
-                    eventArgs.Cancel = true;
-                };
-
-                logger.LogInformation("Starting migrations");
-
-                sp.GetService<ProjectManagementSystemDbContext>().Database.TryMigrate(logger, 10);
-
-                logger.LogInformation("Migrations ended");
-
-                cancellationToken.Token.WaitHandle.WaitOne();
-
-                _manualResetEventSlim.Set();
-            }
-        }
-        
-        private static IConfigurationRoot CreateConfiguration()
-        {
-            return new ConfigurationBuilder()
-                .SetBasePath(Path.Combine(AppContext.BaseDirectory))
-                .AddJsonFile("appsettings.json")
-                .AddEnvironmentVariables()
+                    builder
+                        .AddJsonFile("appsettings.json", true)
+                        .AddEnvironmentVariables()
+                        .AddCommandLine(args);
+                })
+                .ConfigureServices((context, collection) => ConfigureServices(collection, context.Configuration))
+                .UseSerilog((whb, ctx) =>
+                    ctx.ReadFrom.Configuration(whb.Configuration)
+                        .Enrich.FromLogContext())
+                .UseConsoleLifetime()
                 .Build();
+
+            await host.RunAsync();
         }
-        
-        private static IServiceCollection ConfigureServices(IConfiguration configuration)
+
+        private static void ConfigureServices(IServiceCollection serviceCollection, IConfiguration configuration)
         {
-            var services = new ServiceCollection();
-            
-            services.AddLogging(builder => builder
+            serviceCollection.AddLogging(builder => builder
                 .AddSerilog(new LoggerConfiguration()
                     .ReadFrom.Configuration(configuration)
                     .CreateLogger()));
 
-            services.AddDbContext<ProjectManagementSystemDbContext>(options =>
+            serviceCollection.AddDbContext<ProjectManagementSystemDbContext>(options =>
             {
                 options.UseNpgsql(configuration.GetConnectionString("ProjectMS"));
             });
 
-            return services;
-        }
-        
-        public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ProjectManagementSystemDbContext>
-        {
-            public ProjectManagementSystemDbContext CreateDbContext(string[] args)
-            {
-                var configuration = CreateConfiguration();
-                var serviceProvider = ConfigureServices(configuration);
-
-                return serviceProvider.BuildServiceProvider().GetService<ProjectManagementSystemDbContext>();
-            }
+            serviceCollection.AddHostedService<MigrationsBackgroundService>();
         }
     }
 }
