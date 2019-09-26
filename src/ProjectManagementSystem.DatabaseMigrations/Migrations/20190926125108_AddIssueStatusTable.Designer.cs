f530b364a9ab6403415f361a70edf62eba285d24
gradovenko <grad1242@gmail.com>
2019-09-26 18:13:58 +0500

    Add new migration

--- src/ProjectManagementSystem.DatabaseMigrations/Migrations/20190926125108_AddIssueStatusTable.Designer.cs
+++ src/ProjectManagementSystem.DatabaseMigrations/Migrations/20190926125108_AddIssueStatusTable.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using ProjectManagementSystem.DatabaseMigrations;
@@ -9,9 +10,10 @@ using ProjectManagementSystem.DatabaseMigrations;
 namespace ProjectManagementSystem.DatabaseMigrations.Migrations
 {
     [DbContext(typeof(ProjectManagementSystemDbContext))]
-    partial class ProjectManagementSystemDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190926125108_AddIssueStatusTable")]
+    partial class AddIssueStatusTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
